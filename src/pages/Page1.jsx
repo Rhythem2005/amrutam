4f18c261289f042f9dd1adf8e2194d416f38f126
Rhythem2005 <158036736+Rhythem2005@users.noreply.github.com>
2025-08-17 12:06:20 +0530

    updated feature X / fixed bug Y

--- src/pages/Page1.jsx
+++ src/pages/Page1.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../components/navbar'
+import Navbar from "../components/Navbar";
 import Featured from '../components/Featured'
 import Hero from '../components/Hero'
 import WhyChooseUs from '../components/WhyChooseUs'
